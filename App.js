edcec155c2c5f53b181e673c0c18aeb077318ba7
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-20 17:35:48 +0900

    tutorial 10 networking

--- App.js
+++ App.js
@@ -7,7 +7,23 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput, Button, Alert, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, ScrollView, FlatList, SectionList} from 'react-native';
+import {
+  Platform,
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  TextInput, 
+  Button, 
+  Alert, 
+  TouchableHighlight, 
+  TouchableOpacity, 
+  TouchableNativeFeedback, 
+  TouchableWithoutFeedback, 
+  ScrollView,
+  FlatList,
+  ActivityIndicator
+} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,7 +34,11 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
   _onPressButton() {
     Alert.alert('you tapped the button')
   }
@@ -27,19 +47,38 @@ export default class App extends Component<Props> {
     Alert.alert('you long-pressed the button')
   }
 
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
   render() {
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
     return (
-      <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-        />
-      </View>
+      <View style={{flex: 1, paddingTop:20}}>
+      <FlatList
+        data={this.state.dataSource}
+        renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+        keyExtractor={({id}, index) => id}
+      />
+    </View>
     );
   }
 }
@@ -121,19 +160,5 @@ const styles = StyleSheet.create({
   },
   red: {
     color: 'red'
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
+  }
 });
